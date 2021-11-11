5b1c24afd66f7c09320d3f64c42b9577bfa8b308
Ashish <Ashish@DESKTOP-5NHOKSN>
2021-11-11 12:33:55 +0530

    added comments

--- RestProvider/src/main/java/com/copart/RestProviderApplication.java
+++ RestProvider/src/main/java/com/copart/RestProviderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestProviderApplication {
-
+	//added comments
 	public static void main(String[] args) {
 		SpringApplication.run(RestProviderApplication.class, args);
 	}
